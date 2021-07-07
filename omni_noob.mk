3b121d990b0999e9d9854a7298c5cc5c535fab70
Tata Lee <67614323+macukex1991@users.noreply.github.com>
2021-07-08 03:29:47 +0800

    Update omni_noob.mk

--- omni_noob.mk
+++ omni_noob.mk
@@ -1,11 +1,20 @@
 # Release name
 PRODUCT_RELEASE_NAME := noob
 
-# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Pitchblack stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Umidigi
@@ -13,6 +22,7 @@ PRODUCT_DEVICE := noob
 PRODUCT_MANUFACTURER := Umidigi
 PRODUCT_MODEL := a9pro
 PRODUCT_NAME := omni_noob
+
 
 # enable stock zip packages flash
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
